802e7fb037d37df6bf445739d4b3726bb3ef34ad
ali9585 <alikhan9585497@gmail.com>
2024-01-05 18:25:50 +0500

    minor

--- app/Jobs/SendMail.php
+++ app/Jobs/SendMail.php
@@ -35,4 +35,13 @@ class SendMail implements ShouldQueue
         Mail::to($this->recieverEmail)->send(new PrayerUserMail($this->senderName));
     }
 
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array<int, object>
+     */
+    public function middleware(): array
+    {
+        return [new LimitMailJob];
+    }
 }
