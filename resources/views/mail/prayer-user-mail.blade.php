dc219b91e07881e26c73914d67983160ec3cc9bd
ali9585 <alikhan9585497@gmail.com>
2024-01-09 15:23:19 +0500

    minor fixes to the email

--- resources/views/mail/prayer-user-mail.blade.php
+++ resources/views/mail/prayer-user-mail.blade.php
@@ -23,16 +23,22 @@
             {{-- Your Footer Content --}}
             <p style="color: #000000d5; font-size: 15px; font-weight: 700;">
                 PRAY FOR SOMEONE <br>
-                <span style="font-weight: 400; font-size: 13px; line-height: 10px; line-height: 18px; margin-top: 4px;">
-                    {{ $message }}
-                </span>
+                @if ($message != '')
+                    <span style="font-weight: 400; font-size: 13px; line-height: 10px; line-height: 18px; margin-top: 4px;">
+                        {{ $message }}
+                    </span>
+                @endif
             </p>
-            <a href="{{ $iosLink }}">
-                <img src="{{ asset('images/applestore.png') }}" alt="playstore" style="max-width: 146px;">
-            </a>
-            <a href="{{ $androidLink }}">
-                <img src="{{ asset('images/playstore.png') }}" alt="playstore" style="max-width: 160px;">
-            </a>
+            @if ($iosLink != '')
+                <a href="{{ $iosLink }}">
+                    <img src="{{ asset('images/applestore.png') }}" alt="playstore" style="max-width: 146px;">
+                </a>
+            @endif
+            @if ($androidLink != '')
+                <a href="{{ $androidLink }}">
+                    <img src="{{ asset('images/playstore.png') }}" alt="playstore" style="max-width: 160px;">
+                </a>
+            @endif
         @endcomponent
     @endslot
 @endcomponent
