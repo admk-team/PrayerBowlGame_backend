45692dd386da71884b85c93d34f44c9cf133252a
sibihaseb <abdul.haseeb.ali@gmail.com>
2024-05-21 17:03:49 +0500

    Removed Unused Code

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -145,14 +145,7 @@ class AuthController extends Controller
         if ($notification) {
             $notification->delete();
         }
-        // $testimonial = Testimonial::where('user_id', $user->id)->delete();
-        // if ($testimonial) {
-        //     $testimonial->delete();
-        // }
-        // $topwarrior = TopWarrior::where('user_id', $user->id)->delete();
-        // if ($topwarrior) {
-        //     $topwarrior->delete();
-        // }
+
         $user->delete();
         return [
             'message' => 'Account Deleted',
